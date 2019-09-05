3f71b1e68f39c2d6856fb8ce6d8238a9073bd467
Robert Gruner <robert.gruner@jambit.com>
2019-09-05 16:22:31 +0200

    Fetch some movie list on dynamic data page

--- src/DynamicData.tsx
+++ src/DynamicData.tsx
@@ -1,20 +1,62 @@
 import React from 'react';
 import {
+    FlatList,
+    ActivityIndicator,
     View,
     Text,
+    ToastAndroid,
 } from 'react-native';
 
-class DynamicDataScreen extends React.Component {
+interface Movie {
+    id: string;
+    title: string;
+    releaseYear: string;
+}
+
+interface DynamicDataScreenState {
+    isLoading: boolean;
+    dataSource: Movie[];
+}
+
+class DynamicDataScreen extends React.Component<{}, DynamicDataScreenState> {
     static navigationOptions = {
         title: 'Page with dynamic data',
     };
 
+    constructor(props: {}) {
+        super(props);
+        this.state = { isLoading: true, dataSource: [] };
+    }
+
+    componentDidMount() {
+        return fetch('https://facebook.github.io/react-native/movies.json')
+            .then((response) => response.json())
+            .then((responseJson: { movies: Movie[] }) => {
+                this.setState({
+                    isLoading: false,
+                    dataSource: responseJson.movies,
+                }, function () {
+
+                });
+            })
+            .catch((error) => {
+                ToastAndroid.show(`Something went wrong while fetching movies: ${error}`, ToastAndroid.LONG);
+            });
+    }
+
     render() {
-        return (
-            <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-                <Text>Just fancy...shit!</Text>
-            </View>
-        );
+            return (
+                <View style={{ flex: 1, padding: 20 }}>
+                    {this.state.isLoading ?
+                        <ActivityIndicator /> :
+                        <FlatList
+                            data={this.state.dataSource}
+                            renderItem={({ item }) => <Text>{item.title}, {item.releaseYear}</Text>}
+                            keyExtractor={({ id }, index) => id}
+                        />}
+                </View>
+            )
+
     }
 }
 
